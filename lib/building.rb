db440f9f6b99fca069e7a39356c0c3801634387f
Blake Enyart <blake.enyart@gmail.com>
2019-02-06 10:10:49 -0700

    Completion of iteration 4

--- lib/building.rb
+++ lib/building.rb
@@ -11,8 +11,35 @@ class Building
   end
 
   def average_rent
-    all_rent = @units.map { |unit| unit.monthly_rent }
+    all_rent = get_all_rent
     average_rent = all_rent.inject{ |total, monthly_rent| total + monthly_rent }.to_f / all_rent.length
     average_rent.round(1)
+  end
+
+  def get_all_rent
+    @units.map { |unit| unit.monthly_rent }
+  end
+
+  def get_only_full_apartments
+    @units.select { |unit| unit.renter != nil }
+  end
+
+  def renter_with_highest_rent
+    full_units = get_only_full_apartments
+    high_apartment = full_units.max_by { |unit| unit.monthly_rent }
+    high_apartment.renter
+  end
+
+  def annual_breakdown
+    full_units = get_only_full_apartments
+    renters = []
+    annual_rent = []
+    
+    full_units.each do |unit|
+      renters << unit.renter.name
+      annual_rent << unit.monthly_rent*12
+    end
+
+    Hash[renters.zip(annual_rent)]
   end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -12,6 +12,7 @@ class BuildingTest < Minitest::Test
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @jessie = Renter.new('Jessie')
+    @spencer = Renter.new("Spencer")
   end
 
   def test_building_exist
@@ -36,8 +37,34 @@ class BuildingTest < Minitest::Test
   def test_average_rent_calculated_from_all_units_in_building
     @building.add_unit(@a1)
     @building.add_unit(@b2)
-    
+
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_renter_with_highest_rent_applys_only_to_filled_units
+    @b2.add_renter(@spencer)
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+
+    assert_equal @spencer, @building.renter_with_highest_rent
+
+    @a1.add_renter(@jessie)
+
+    assert_equal @jessie, @building.renter_with_highest_rent
+  end
+
+  def test_annual_breakdown_creates_hash_renter_and_annual_rent
+    @b2.add_renter(@spencer)
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+    expected = {"Spencer" => 11988}
+
+    assert_equal expected, @building.annual_breakdown
+
+    @a1.add_renter(@jessie)
+    expected = {"Jessie" => 14400, "Spencer" => 11988}
+
+    assert_equal expected, @building.annual_breakdown
   end
 end
 
